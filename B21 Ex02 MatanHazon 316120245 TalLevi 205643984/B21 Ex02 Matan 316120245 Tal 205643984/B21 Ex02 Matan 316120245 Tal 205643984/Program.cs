82e47c01de2e2599f07f7b7a022979f8f7ab89e3
tallevi120 <tallevi120@gmail.com>
2021-04-27 18:28:05 +0300

    Create Program.cs

---
+++ B21 Ex02 MatanHazon 316120245 TalLevi 205643984/B21 Ex02 Matan 316120245 Tal 205643984/B21 Ex02 Matan 316120245 Tal 205643984/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B21_Ex02_Matan_316120245_Tal_205643984
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
