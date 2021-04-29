29cb9bf584038001c2e2d2ad9428a4e1d18bfbef
tallevi120 <tallevi120@gmail.com>
2021-04-29 17:56:04 +0300

    Create HumanPlayer.cs

---
+++ B21 Ex02 MatanHazon 316120245 TalLevi 205643984/B21 Ex02 Matan 316120245 Tal 205643984/B21 Ex02 Matan 316120245 Tal 205643984/HumanPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B21_Ex02_Matan_316120245_Tal_205643984
+{
+    struct HumanPlayer
+    {
+        private string m_PlayerName;
+        private string m_Mark;
+        private int m_Wins;
+
+
+
+        HumanPlayer(string _playerName , string _mark)
+        {
+            m_PlayerName = _playerName;
+            m_Mark = _mark;
+            m_Wins = 0;
+        }
+
+
+    }
+}
