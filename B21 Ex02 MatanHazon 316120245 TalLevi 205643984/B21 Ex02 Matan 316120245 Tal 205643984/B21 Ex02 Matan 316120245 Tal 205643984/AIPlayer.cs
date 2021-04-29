7e4cd5b20d8b97349e69ece4ab6e60e9fff9aa1a
tallevi120 <tallevi120@gmail.com>
2021-04-29 17:56:25 +0300

    Create AIPlayer.cs

---
+++ B21 Ex02 MatanHazon 316120245 TalLevi 205643984/B21 Ex02 Matan 316120245 Tal 205643984/B21 Ex02 Matan 316120245 Tal 205643984/AIPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B21_Ex02_Matan_316120245_Tal_205643984
+{
+    class AIPlayer
+    {
+        string playerName;
+        string mark;
+        int wins;
+    }
+}
